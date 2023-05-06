4cd20d4f219f5c6028caeb467acafaee022bc8e6
Owner <rmsqh4617@gmail.com>
2023-05-07 00:20:51 +0900

    Fix: 날짜 null값 해결

--- src/test/java/com/sparta/meme_nameking/MemeNameKingApplicationTests.java
+++ src/test/java/com/sparta/meme_nameking/MemeNameKingApplicationTests.java
@@ -2,9 +2,7 @@ package com.sparta.meme_nameking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootTest
 class MemeNameKingApplicationTests {
 
