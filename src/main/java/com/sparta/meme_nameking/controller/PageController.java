0ed12706bac6330e768e7996c20be936fa5796f4
Owner <rmsqh4617@gmail.com>
2023-05-07 02:58:48 +0900

    Feat: PageController 프레임생성
    기능은 미구현 입니다.

---
+++ src/main/java/com/sparta/meme_nameking/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.sparta.meme_nameking.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PageController {
+
+
+
+
+}
--- src/main/java/com/sparta/meme_nameking/dto/PageResponseDto.java
+++ src/main/java/com/sparta/meme_nameking/dto/PageResponseDto.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-public class PagesResponseDto {
+public class PageResponseDto {
 
     @Getter
     @NoArgsConstructor
