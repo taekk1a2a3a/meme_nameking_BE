d39cf60004d7a5312bc39f0a84635328e3f515bf
limseulki <limslki333@hanmail.net>
2023-05-07 02:09:10 +0900

    Fix: 이미지 upLoadLocation 수정 및 API uri 수정
    이미지 upLoadLocation(절대경로) application.properties에 설정
    REST API로 수정

--- src/main/java/com/sparta/meme_nameking/controller/PostController.java
+++ src/main/java/com/sparta/meme_nameking/controller/PostController.java
@@ -1,12 +1,13 @@
 package com.sparta.meme_nameking.controller;
 
-import com.sparta.meme_nameking.dto.PostRequestDto;
 import com.sparta.meme_nameking.dto.ResponseMsgDto;
 import com.sparta.meme_nameking.security.UserDetailsImpl;
 import com.sparta.meme_nameking.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,20 +19,20 @@ public class PostController {
     private final PostService postService;
 
     // 게시글 작성
-    @PostMapping("/post/create")
+    @PostMapping("/posts")
     public ResponseMsgDto<?> createPost(@RequestParam(value = "image") MultipartFile image, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
         return postService.createPost(image, userDetails.getUser());
     }
 
 //    // 게시글 수정
-//    @PutMapping("/detail/post/update/{post-id}")
-//    public ResponseMsgDto<?> updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//    @PutMapping("/posts/{post-id}")
+//    public ResponseMsgDto<?> updatePost(@PathVariable(name = "{post-id}") Long id, @RequestBody PostRequestDto postRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 //        return postService.updatePost(id, postRequestDto, userDetails.getUser());
 //    }
 //
 //    // 게시글 삭제
-//    @DeleteMapping("/detail/post/delete/{post-id}")
-//    public ResponseMsgDto<?> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//    @DeleteMapping("/posts/{post-id}")
+//    public ResponseMsgDto<?> deletePost(@PathVariable(name = "{post-id}") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 //        return postService.deletePost(id, userDetails.getUser());
 //    }
 }
