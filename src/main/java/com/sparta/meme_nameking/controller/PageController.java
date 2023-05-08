193ea9448238ad7144a980161e55b865f040e62d
Owner <rmsqh4617@gmail.com>
2023-05-08 14:59:04 +0900

    Feat: Detailpage 상단 하단 프레임 추가,
    상세 페이지 조회시, 베스트 댓글 3개 추출하고
    나머지 댓글들을 정렬한 다음,
    상세 페이지 응답 DTO를 각 각 생성해서
    응답하게끔 구현

--- src/main/java/com/sparta/meme_nameking/controller/PageController.java
+++ src/main/java/com/sparta/meme_nameking/controller/PageController.java
@@ -6,6 +6,7 @@ import com.sparta.meme_nameking.service.PageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,12 @@ public class PageController {
     @GetMapping("/posts")
     public ResponseMsgDto allPageLoad(@AuthenticationPrincipal UserDetailsImpl userDetails){
         return pageService.allPageLoad(userDetails.getUser());
+    }
+
+    // 상세 페이지 조회
+    @GetMapping("/posts/{postId}")
+    public ResponseMsgDto detailPageLoad(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return pageService.detailPageLoad(postId, userDetails.getUser());
     }
 
 
--- src/main/java/com/sparta/meme_nameking/dto/ResponseMsgDto.java
+++ src/main/java/com/sparta/meme_nameking/dto/ResponseMsgDto.java
@@ -6,18 +6,24 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ResponseMsgDto<T> {
+public class ResponseMsgDto<T, U> {
 
     private int status;
     private String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private U data2;
 
-    public static <T> ResponseMsgDto<T> setSuccess(int status, String message, T data){
-        return new ResponseMsgDto<>(status, message, data);
+    public static <T> ResponseMsgDto<T, ?> setSuccess(int status, String message, T data){
+        return new ResponseMsgDto<>(status, message, data, null);
     }
 
-    public static <T> ResponseMsgDto<T> setFail(int status, String message){
-        return new ResponseMsgDto<>(status, message, null);
+    public static <T, U> ResponseMsgDto<T, U> setSuccess(int status, String message, T data, U data2){
+        return new ResponseMsgDto<>(status, message, data, data2);
+    }
+
+    public static <T> ResponseMsgDto<T, ?> setFail(int status, String message){
+        return new ResponseMsgDto<>(status, message, null, null);
     }
 }
