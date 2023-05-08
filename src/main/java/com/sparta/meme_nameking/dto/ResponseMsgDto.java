efe9d064332b0c967b9a55f42e2f5f9cd4a40051
Owner <rmsqh4617@gmail.com>
2023-05-08 15:23:23 +0900

    Feat: Detailpage 상단 하단 프레임 추가,
    상세 페이지 조회시, 베스트 댓글 3개 추출하고
    나머지 댓글들을 정렬한 다음,
    상세 페이지 응답 DTO를 각 각 생성해서
    응답하게끔 구현

--- src/main/java/com/sparta/meme_nameking/dto/ResponseMsgDto.java
+++ src/main/java/com/sparta/meme_nameking/dto/ResponseMsgDto.java
@@ -6,24 +6,18 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ResponseMsgDto<T, U> {
+public class ResponseMsgDto<T> {
 
     private int status;
     private String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private U data2;
 
-    public static <T> ResponseMsgDto<T, ?> setSuccess(int status, String message, T data){
-        return new ResponseMsgDto<>(status, message, data, null);
+    public static <T> ResponseMsgDto<T> setSuccess(int status, String message, T data){
+        return new ResponseMsgDto<>(status, message, data);
     }
 
-    public static <T, U> ResponseMsgDto<T, U> setSuccess(int status, String message, T data, U data2){
-        return new ResponseMsgDto<>(status, message, data, data2);
-    }
-
-    public static <T> ResponseMsgDto<T, ?> setFail(int status, String message){
-        return new ResponseMsgDto<>(status, message, null, null);
+    public static <T> ResponseMsgDto<T> setFail(int status, String message){
+        return new ResponseMsgDto<>(status, message, null);
     }
 }
