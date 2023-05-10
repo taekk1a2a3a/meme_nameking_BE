7ff450997ec4bdb2280808406c464c0331813f90
KGB <125139072+CaptainGombo@users.noreply.github.com>
2023-05-11 06:09:11 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/app/zip/build/libs/"
-JAR="$ROOT_PATH/Meme_NameKing-0.0.1-SNAPSHOT.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
+ROOT_PATH=/home/ubuntu/app/zip/build/libs/
+JAR=$ROOT_PATH/Meme_NameKing-0.0.1-SNAPSHOT.jar
+STOP_LOG=$ROOT_PATH/stop.log
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
