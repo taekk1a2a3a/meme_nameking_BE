3c250f49a8ebd6d7265f92159240393ce6e6e4ab
KGB <125139072+CaptainGombo@users.noreply.github.com>
2023-05-11 06:20:03 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ROOT_PATH=/home/ubuntu/app/zip/build/libs/
-JAR=$ROOT_PATH/Meme_NameKing-0.0.1-SNAPSHOT.jar
-STOP_LOG=$ROOT_PATH/stop.log
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+ROOT_PATH="/home/ubuntu/app/zip"
+APPLICATION_JAR="$ROOT_PATH/Meme_NameKing-0.0.1-SNAPSHOT.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $APPLICATION_JAR) # 실행 중인 Spring Boot 애플리케이션의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-echo "서비스 NouFound" >> $STOP_LOG
+echo "현재 실행 중인 서비스가 없습니다." >> $STOP_LOG
 else
-echo "서비스 종료 " >> $STOP_LOG
+echo "서비스를 종료합니다." >> $STOP_LOG
 kill "$SERVICE_PID"
-# kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+# kill -9 $SERVICE_PID # 강제 종료를 원할 경우 이 명령어를 사용하세요.
 fi
