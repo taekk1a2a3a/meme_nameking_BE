c2ebfe217f87afe320e7dfd7524e0ff3777da6a1
KGB <125139072+CaptainGombo@users.noreply.github.com>
2023-05-11 07:19:47 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/app/zip"
+ROOT_PATH="/home/ubuntu/app/zip/build/libs/"
 APPLICATION_JAR="$ROOT_PATH/Meme_NameKing-0.0.1-SNAPSHOT.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $APPLICATION_JAR) # 실행 중인 Spring Boot 애플리케이션의 PID
